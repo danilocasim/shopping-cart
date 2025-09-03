3f4397659616e758eade247167a6ebd32967b9e1
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-03 14:00:55 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./styles/App.css";
+import { HelloWorld } from "./components/HelloWorld";
+
+export function App() {
+  return <HelloWorld name={"World"}></HelloWorld>;
+}
+
+export default App;
