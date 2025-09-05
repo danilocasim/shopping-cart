bf500af77327bb152ea587045b5c50b52a9470ff
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-05 10:46:28 +0800

    fix: the test for navigation by implementing a memory router inside the test

--- src/tests/components/Navigation.test.jsx
+++ src/tests/components/Navigation.test.jsx
@@ -1,12 +1,36 @@
 import { describe, it, expect } from "vitest";
+import { RouterProvider, createMemoryRouter } from "react-router";
 
 import { render, screen } from "@testing-library/react";
-import Navigation from "../../components/Navigation/Navigation.jsx";
+import App from "../../App.jsx";
+import ErrorPage from "../../pages/ErrorPage.jsx";
+import Products from "../../pages/Products.jsx";
+import Checkout from "../../pages/Checkout.jsx";
+import Homepage from "../../pages/Homepage.jsx";
 
 describe("Navigation Component", () => {
   it("renders the content correctly", () => {
-    render(<Navigation />);
+    const routes = [
+      {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Homepage /> },
+          { path: "products", element: <Products /> },
+          { path: "checkout", element: <Checkout /> },
+        ],
+      },
+    ];
+    const router = createMemoryRouter(routes);
 
-    expect(screen.getByText("heading").textContent).toMatch(/Shoplifts/);
+    render(<RouterProvider router={router}></RouterProvider>);
+
+    expect(
+      screen.getByRole("heading", { name: "Shoplifts" })
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole("link", { name: "Products" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Checkout" })).toBeInTheDocument();
   });
 });
