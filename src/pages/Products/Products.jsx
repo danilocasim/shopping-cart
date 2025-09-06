74ec40f2aee0537b6ff294671b92f4bd0db40fe9
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-06 12:20:38 +0800

    feat: add a functionality by getting the data, loading, error from hook then display the image data

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,7 +3,30 @@ import { useStoreAPI } from "../../components/hooks/useStoreAPI";
 import style from "./Products.module.css";
 
 function Products() {
-  return <h1>Products</h1>;
+  const [productsData, loading, error] = useStoreAPI();
+
+  console.log(productsData);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  return (
+    <div className={style.products}>
+      <h1>Products</h1>
+      {productsData &&
+        productsData.map((data) => {
+          return (
+            <div className={style.productWrapper} key={data.id}>
+              <img
+                className={style.productImage}
+                src={data.image}
+                alt={data.title}
+              />
+
+              <p>{data.title}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
 export default Products;
