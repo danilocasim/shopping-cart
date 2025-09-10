4e97b4b1e2236cdfcb43c27331392aeb0c4dd302
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-10 15:27:22 +0800

    feat: render all of the checkoutItems

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,5 +1,30 @@
+import { Fragment } from "react";
+import { useOutletContext } from "react-router";
+import style from "./Checkout.module.css";
 function Checkout() {
-  return <h1>Checkout</h1>;
+  const [checkoutItems, setCheckoutItems] = useOutletContext();
+
+  return (
+    <div>
+      <h1>Checkout</h1>
+      {checkoutItems &&
+        checkoutItems.map((item) => {
+          return (
+            <Fragment key={item.id}>
+              <img
+                className={style.productImage}
+                key={item.title}
+                src={item.image}
+                alt={item.title}
+                data-testid={item.title}
+              />
+              <p>{item.title}</p>
+              <p>{item.quantity}</p>
+            </Fragment>
+          );
+        })}
+    </div>
+  );
 }
 
 export default Checkout;
