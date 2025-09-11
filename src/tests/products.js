4915a69a9e6a3f9859fffd85a0d5eacd4269d16b
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-11 19:08:39 +0800

    chore: add mock values of fetched data products

---
+++ src/tests/products.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: 1,
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+  },
+  {
+    id: 2,
+    price: 22.3,
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+    title: "Mens Casual Premium Slim Fit T-Shirts ",
+  },
+  {
+    id: 3,
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+    price: 55.99,
+    title: "Mens Cotton Jacket",
+  },
+];
+
+export { products };
