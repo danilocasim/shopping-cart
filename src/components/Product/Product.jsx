bb84c18c9f91e1fd2a5295bb6ce135d807e3d889
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-13 14:58:55 +0800

    style: make the product component more visual appealing

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,26 +5,35 @@ function Product({ data, onClick, onChange, increment, decrement }) {
     <div className={style.productWrapper}>
       <img className={style.productImage} src={data.image} alt={data.title} />
 
-      <p>{data.title}</p>
-      <div className={style.buttonWrapper}>
-        <button onClick={decrement} data-testid={data.id + "-decrement"}>
-          -
-        </button>
-        <input
-          min={1}
-          maxLength={3}
-          value={data.quantity}
-          data-testid={data.id + "-quantity"}
-          type='tel'
-          onChange={onChange}
-        />
-        <button onClick={increment} data-testid={data.id + "-increment"}>
-          +
-        </button>
+      <div className={style.description}>
+        <p>{data.title}</p>
+        <p>${data.price}</p>
+        <div className={style.buttonWrapper}>
+          <div className={style.quantityBtn}>
+            <button onClick={decrement} data-testid={data.id + "-decrement"}>
+              -
+            </button>
+            <input
+              min={1}
+              maxLength={3}
+              value={data.quantity}
+              data-testid={data.id + "-quantity"}
+              type='tel'
+              onChange={onChange}
+            />
+            <button onClick={increment} data-testid={data.id + "-increment"}>
+              +
+            </button>
+          </div>
+          <button
+            className={style.addToCartBtn}
+            onClick={onClick}
+            data-testid={data.title}
+          >
+            Add to Cart
+          </button>
+        </div>
       </div>
-      <button onClick={onClick} data-testid={data.title}>
-        Add to Cart
-      </button>
     </div>
   );
 }
