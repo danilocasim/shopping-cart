b2bf32e73ccbbc0aa677ada4e08ef341fa77ec25
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-13 15:04:09 +0800

    feat: add the functionality for checkout such as delete, increment, decrement quantity and remove all product by checking out

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -4,19 +4,50 @@ import style from "./Checkout.module.css";
 function Checkout() {
   const [checkoutItems, setCheckoutItems] = useOutletContext();
 
+  function decrement(index) {
+    const newCheckoutItems = [...checkoutItems];
+
+    newCheckoutItems[index].quantity--;
+
+    if (newCheckoutItems[index].quantity === 0) {
+      newCheckoutItems.splice(index, 1);
+    }
+    setCheckoutItems(newCheckoutItems);
+  }
+
+  function increment(index) {
+    const newCheckoutItems = [...checkoutItems];
+    newCheckoutItems[index].quantity++;
+
+    setCheckoutItems(newCheckoutItems);
+  }
+
+  function deleteProduct(index) {
+    const newCheckoutItems = [...checkoutItems];
+    newCheckoutItems.splice(index, 1);
+    setCheckoutItems(newCheckoutItems);
+  }
+
+  function removeAllProducts() {
+    setCheckoutItems([]);
+  }
+
+  if (checkoutItems.length === 0)
+    return <h1 className={style.emptyMessage}>YOUR CART IS EMPTY</h1>;
+
   return (
-    <div>
-      <h1>Checkout</h1>
+    <Fragment>
+      <div className={style.checkoutWrapper}>
+        <div className={style.checkoutProducts}>
       {checkoutItems &&
-        checkoutItems.map((item) => {
+            checkoutItems.map((item, index) => {
           return (
             <Fragment key={item.id}>
-              <img
-                className={style.productImage}
-                key={item.title}
-                src={item.image}
-                alt={item.title}
-                data-testid={item.title}
+                  <CheckoutProduct
+                    item={item}
+                    decrement={() => decrement(index)}
+                    increment={() => increment(index)}
+                    deleteProduct={() => deleteProduct(index)}
               />
               <p>{item.title}</p>
               <p>{item.quantity}</p>
