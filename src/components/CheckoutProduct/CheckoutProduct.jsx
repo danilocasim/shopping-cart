e08df56d31067efc1c1bde490fced142fca8a8f3
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-09-13 14:55:44 +0800

    feat: add checkout product component and render the data of it

---
+++ src/components/CheckoutProduct/CheckoutProduct.jsx
@@ -0,0 +1,52 @@
+import style from "./CheckoutProduct.module.css";
+
+function CheckoutProduct({ item, decrement, increment, deleteProduct }) {
+  return (
+    <div className={style.checkoutProduct}>
+      <img
+        className={style.productImage}
+        key={item.title}
+        src={item.image}
+        alt={item.title}
+        data-testid={item.title}
+      />
+
+      <div className={style.description}>
+        <div className={style.row}>
+          <p>{item.title}</p>
+          <button
+            data-testid={item.id + "-delete-product"}
+            onClick={deleteProduct}
+            className={style.delete}
+          >
+            X
+          </button>
+        </div>
+        <div className={style.buttonWrapper}>
+          <button
+            onClick={decrement}
+            data-testid={item.id + "-decrement-checkout"}
+          >
+            -
+          </button>
+          <input
+            min={1}
+            maxLength={3}
+            value={item.quantity}
+            data-testid={item.id + "-quantity-checkout"}
+            type='tel'
+          />
+          <button
+            onClick={increment}
+            data-testid={item.id + "-increment-checkout"}
+          >
+            +
+          </button>
+        </div>
+        <p className={style.price}>${item.price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
